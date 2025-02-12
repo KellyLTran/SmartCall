14a814aca0eaa97f1f09965a399cffa32ac6dc1f
Kelly Tran <trank@ufl.edu>
2025-02-12 15:55:35 -0500

    Implement user registration and tournament views

--- smart_call_app/forms.py
+++ smart_call_app/forms.py
@@ -1,13 +1,29 @@
 from django import forms
-from .models import Duel
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Tournament, Duel
 
-# Form that allow users to create a new Duel by inputting two phones
+# Form for user registration
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+# Form for creating a new tournament 
+class TournamentForm(forms.ModelForm):
+    class Meta:
+        model = Tournament
+        fields = ['name']
+
+# Form for creating a new Duel by inputting two phones
 class DuelForm(forms.ModelForm):
     class Meta:
         model = Duel
         fields = ["phone_1", "phone_2"]
 
-# Form that allow users to select the winner of the current duel
+# Form for selecting the winner of the current duel
 class WinnerForm(forms.ModelForm):
     class Meta:
         model = Duel
--- smart_call_app/models.py
+++ smart_call_app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Model to represent each full tournament
 class Tournament(models.Model):
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.name
 
 # Model to represent each pair of phones competing with each other (duels)
 class Duel(models.Model):
